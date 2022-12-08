5bc53db5d1c2b01a1fe3af8429a72e01006fd61f
meseret1918 <meseret1918@gmail.com>
2022-12-08 09:54:09 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - Entry point
- *Return: Always 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
