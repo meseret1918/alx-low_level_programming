dccd913d3079f4a83bac951c728154f1b8f558cc
meseret1918 <meseret1918@gmail.com>
2022-12-08 07:45:25 -0800

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * eturn: Always 1 (Success)
+ */
+int main(void)
+
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
