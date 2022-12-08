350bf9a7eb68762074f7b18018a9d0357952f4ed
meseret1918 <meseret1918@gmail.com>
2022-12-08 06:05:23 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
+}
+
