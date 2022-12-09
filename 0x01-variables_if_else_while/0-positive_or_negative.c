16e55e1a1571d4eab1e9263dda6cc516cf88bfb5
meseret1918 <meseret1918@gmail.com>
2022-12-09 01:19:38 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/**
+ * main prints the last digit of arandom generating number, whether
+ *   >5, <6 or 0.
+ *  return: always 0.
+ */
 int main(void)
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%i is positive\n", n);
