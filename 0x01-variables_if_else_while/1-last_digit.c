bef5250fda9b8fbdb93b58f29cd8331037e1ef1b
meseret1918 <meseret1918@gmail.com>
2022-12-09 02:19:28 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -26,7 +26,6 @@ int main(void)
 	{
 		printf("Last digit of %i is %i and is 0\n", n, n % 10);
 	}
-
 	return (0);
 }
 
