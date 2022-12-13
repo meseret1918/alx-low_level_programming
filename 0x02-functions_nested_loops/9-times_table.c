dc58a9337b550bf75df4b6ec30b0c6aa5e84e46a
meseret1918 <meseret1918@gmail.com>
2022-12-13 10:22:47 -0800

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times, starting with 0.
+ */
+void times_table(void)
+{
+	int num, mult, product;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = num * mult;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
+				
+				_putchar((product / 10) + '0');
+
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
