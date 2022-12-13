223183a68a58e9b190ed5cf522554c3f578852d8
meseret1918 <meseret1918@gmail.com>
2022-12-13 14:28:29 -0800

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
@@ -38,6 +38,7 @@ int main (void)
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
+
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
