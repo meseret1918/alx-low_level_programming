d2bbf6480b3b43a0794a32db52fb4134918be0c5
meseret1918 <meseret1918@gmail.com>
2022-12-15 03:39:35 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numberfrom 0 to 9
+ *
+ * Return: 0-9 followed by a new line
+ */
+void print_numbers(void)
+{
+	int z;
+
+	for (z = 48; z <= 57; z++)
+	{
+		_putchar(z);
+	}
+	_putchar('\n');
+}
