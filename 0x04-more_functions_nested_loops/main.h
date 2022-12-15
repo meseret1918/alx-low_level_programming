458d9ce27368e03794a5a55ea8acb6b3abfccf64
meseret1918 <meseret1918@gmail.com>
2022-12-15 00:10:17 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MESERET_H_INCLUDE
+#define MESERET_H_INCLUDE
+
+/**
+ * file: main.h
+ *
+ * desc:header file used in 0x04-more_functions_nested_loops directory.
+ */
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
