6608bd3a8138eeaec4b3ca85dd190d5efc29a62b
meseret1918 <meseret1918@gmail.com>
2022-12-15 04:30:10 -0800

    task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers - a function that prints 10 times the numbers, from 0 to 14
- * Return: 0 - 14 followed by a new line
+ * more_numbers - a function that prints 10 times the numbers
+ * from 0 to 14
+ * Return: 10 times of the the numbers
  */
 void more_numbers(void)
 {
