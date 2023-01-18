56f58c577fbe35b907394f8f3949479836bb1866
meseret1918 <meseret1918@gmail.com>
2023-01-18 04:08:41 -0800

    task 0- 3

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - a function that prints strings
+ * @separator: comma space
+ * @n: number of elements
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *sep, *ptr;
+	unsigned int i;
+	va_list list;
+
+	if (separator == NULL || *separator == 0)
+		sep = "";
+	else
+		sep = (char *) separator;
+	va_start(list, n);
+
+	if (n > 0)
+		printf("%s", va_arg(list, char *));
+	for (i = 1; i < n; i++)
+	{
+		ptr = va_arg(list, char*);
+		if (ptr == NULL)
+			ptr = "(nil)";
+		printf("%s%s", sep, ptr);
+	}
+	printf("\n");
+	va_end(list);
+}
