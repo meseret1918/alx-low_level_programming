5ef48ad2ed380c7a8e72951cf948a0e815252123
meseret1918 <meseret1918@gmail.com>
2023-01-26 05:03:11 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-	int m;
+	size_t nodes = 0;
 
-	for (m = 0; h != NULL; m++)
+	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
+
 		else
-		{
-			printf("[%i] %s\n", h->len, h->str);
-		}
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
 	}
-	return (m);
+
+	return (nodes);
 }
