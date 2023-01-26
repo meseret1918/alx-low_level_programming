8522dcd9d753321840685114dbb6ee52309629e2
meseret1918 <meseret1918@gmail.com>
2023-01-26 10:31:26 -0800

    3. Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,35 +1,45 @@
 #include "lists.h"
 
-int _strlen_recursion(char *s);
-
 /**
- * add_node_end - a function that adds a new node at the end of a list_t list
- * @head: pointer to the first element of the list
- * @str: string to set in the new node
-g*
- * Return: address of the new element, or NULL if it failed
- **/
+ * *add_node_end - function adds new node to end of list
+ *
+ *@head: pointer to pointer of head of linked list
+ *@str: const char pointer to string
+ *
+ * Return: address of new element, or NULL if failed
+ */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	char *nstr = strdup(str);
-	list_t *newhead = NULL;
-	int m;
+	list_t *last = *head;
+	list_t *new_node = NULL;
+	int x;
 
 	if (!nstr)
 		return (NULL);
-	for (m = 0; nstr[m] != '\0'; m++)
-	{
+	for (x = 0; nstr[x] != '\0'; x++)
 		;
-	}
-	newhead = malloc(sizeof(list_t));
-	if (newhead == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 	{
 		free(nstr);
 		return (NULL);
 	}
-	newhead->str = nstr;
-	newhead->len = m;
-	newhead->next = *head;
-	*head = newhead;
-	return (newhead);
+	new_node->str = nstr;
+	new_node->len = x;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = new_node;
+	return (new_node);
 }
