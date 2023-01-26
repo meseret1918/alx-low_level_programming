25ad87b91e540e8819425de7bb6c6089298b58aa
meseret1918 <meseret1918@gmail.com>
2023-01-26 09:29:22 -0800

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,6 +3,7 @@
 /**
  * list_len - a function returns the number of elements in a linked list_t list
  * @h: pointer to the list
+ *
  * Return: number of nodes
  **/
 size_t list_len(const list_t *h)
