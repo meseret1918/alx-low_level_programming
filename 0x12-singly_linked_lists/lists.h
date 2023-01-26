1b9712a5e09c2afc29f1cb4847e6d81beb71a5a5
meseret1918 <meseret1918@gmail.com>
2023-01-26 09:15:53 -0800

    update linkid list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,15 +11,15 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
-i *
  * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}
+list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
