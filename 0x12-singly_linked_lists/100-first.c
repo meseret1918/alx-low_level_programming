f1dd5de65aedb24cccf9127860cbe67ba25e80fe
meseret1918 <meseret1918@gmail.com>
2023-01-26 10:05:01 -0800

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 /**
- * first - prints before main function is executed
+ * first - Write a function that prints You're beat! and yet,
+ * you must allow,\nI bore my house upon my back!\n before the main
+ * function is executed.
  */
 void __attribute__ ((constructor)) first()
 {
