eb2eb30b71f56aa15511c686e5d9b500d61806ae
meseret1918 <meseret1918@gmail.com>
2023-02-02 05:01:38 -0800

    task 0-5

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,22 +6,22 @@
  *
  * Return: the converted number, or 0 if
  * there is one or more chars in the string b that is not 0 or 1
- * b is NULL 
+ * b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int n = 0;
+	unsigned int n = 0;
 
-    if (b == NULL)
-	    return (0);
-    while (*b != '\0')
-    {
-	    n = n << 1;
-	    if (*b != '1' && *b != '0')
-		    return (0);
-	    else if (*b == '1')
-		    n = n | 1;
-	    b++;
-    }
-    return (n);
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		n = n << 1;
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			n = n | 1;
+		b++;
+	}
+	return (n);
 }
