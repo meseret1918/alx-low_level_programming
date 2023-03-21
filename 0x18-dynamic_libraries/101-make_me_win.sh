8bb2373d9a2ea21dc7bd1feeffdb79028c687018
meseret1918 <meseret1918@gmail.com>
2023-03-21 05:01:29 -0700

    task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -o /tmp/test.so/https:[//raw.github.com/meseret1918/alx-low_level_programmi                            ng/tree/master/0x18-dynamic_libraries/test.so]
+wget -O /tmp/test.so https://github.com/meseret1918/alx-low_level_programming/raw/master/0x17-dynamic_libraries/test.so
 export LD_PRELOAD=/tmp/test.so
