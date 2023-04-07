5bd5079dbda767c4bb20178c723aeef572623261
meseret1918 <meseret1918@gmail.com>
2023-04-07 12:24:54 -0700

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
+
 	hashtable = malloc(sizeof(hash_table_t));
+
 	if (hashtable == NULL)
 		return (NULL);
+
 	hashtable->size = size;
 	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (hashtable->array == NULL)
 		return (NULL);
 
